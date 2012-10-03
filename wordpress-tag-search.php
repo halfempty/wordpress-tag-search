222c645b3df0cfedfb9e85a29a6cd0e1f2e7d8a5
halfempty.com <halfempty.com@cl40.gs01.gridserver.com>
2012-10-03 15:50:52 -0700

    Work on #1

--- wordpress-tag-search.php
+++ wordpress-tag-search.php
@@ -9,6 +9,30 @@ Author URI: http://martyspellerberg.com
 License: GPLv2+
 */
 
+
+// Load TDOTF
+require_once(dirname(__FILE__) . '/tdotf/tdotf.php');
+
+
+// Load assets
+define('MY_PLUGIN_FOLDER',str_replace("\\",'/',dirname(__FILE__)));
+
+function mca_gallery_scripts_method() {
+
+	wp_enqueue_script("jquery");
+
+    $tagsearchcss = plugins_url( '/assets/tagsearch.css' , __FILE__ );
+    wp_register_style('tagsearchcss',$tagsearchcss);
+    wp_enqueue_style( 'tagsearchcss');
+
+	$tagsearchjs = plugins_url( '/assets/tagsearch.js' , __FILE__ );
+	wp_register_script('tagsearchjs',$tagsearchjs);
+	wp_enqueue_script( 'tagsearchjs',array('jquery'));
+}
+
+add_action('wp_enqueue_scripts', 'mca_gallery_scripts_method');
+
+
 function wp_tag_search() {
  
 	$tags = get_tags();
